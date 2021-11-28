03a84afd2df2ff6fac3a8b6943abfc907dbb8b9c
r0mille <ke4mtg+r0mille@gmail.com>
2021-11-28 11:09:02 -0700

    push what i have so elbrocko can save me

--- components/image_generation/backgroundSVG.tsx
+++ components/image_generation/backgroundSVG.tsx
@@ -1,42 +1,37 @@
-import styled from "styled-components";
-import { ReactSVG } from "react-svg";
+//import styled from "styled-components";
 import RND from "./randomizer";
+//import { renderToString } from "react-dom/server";
 
-const BackgroundComponent = styled((props) => {
+// Todo: would be great to dynamically load the svg after it's been determined
+// @ts-ignore
+import greenBackground from "../../public/assets/raging-santas/1000_background_green.svg";
+// @ts-ignore
+import blueBackground from "../../public/assets/raging-santas/1000_background_baby_blue.svg";
+// @ts-ignore
+import pinkBackground from "../../public/assets/raging-santas/1000_background_pink.svg";
+
+const BackgroundComponent = props => {
   const roll = props.rnd.rb(0, 1);
 
-  let color = "baby_blue";
+  let color = blueBackground;
   switch (true) {
     case roll < 0.333:
-      color = "green";
+      color = greenBackground;
       break;
     case roll < 0.666:
-      color = "pink";
+      color = pinkBackground;
       break;
     default:
   }
 
-  return (
-    <ReactSVG
-      src={`assets/raging-santas/1000_background_${color}.svg`}
-      renumerateIRIElements={false}
-      wrapper="svg"
-      {...props}
-    />
-  );
-})`
-  position: absolute;
-  left: 0;
-  top: 0;
-
-  svg {
-    height: 478px;
-  }
-`;
+  // Want to return pure string but renderToString escapes the html stuff
+  // return renderToString(color);
+  return color;
+}
 
 const BackgroundS = ({ seed }) => {
   const rnd = new RND(parseInt((142442 * seed).toString()));
-  return <BackgroundComponent rnd={rnd} />;
+  return BackgroundComponent({rnd});
 };
 
 const Background = BackgroundS;
--- components/image_generation/svg-sketch.tsx
+++ components/image_generation/svg-sketch.tsx
@@ -1,20 +1,21 @@
 import styled from "styled-components";
 
-import RND from "./randomizer";
-
-import Arms from "./armsSVG";
 import Background from "./backgroundSVG";
-import Beard from "./beardSVG";
-import Body from "./bodySVG";
-import Brows from "./browsSVG";
-import Eyes from "./eyesSVG";
-import Glasses from "./glassesSVG";
-import Hat from "./hatSVG";
-import Head from "./headSVG";
-import Mouth from "./mouthSVG";
-import Nose from "./noseSVG";
 
-const CanvasDiv = styled.svg`
+//import RND from "./randomizer";
+
+//import Arms from "./armsSVG";
+//import Beard from "./beardSVG";
+//import Body from "./bodySVG";
+//import Brows from "./browsSVG";
+//import Eyes from "./eyesSVG";
+//import Glasses from "./glassesSVG";
+//import Hat from "./hatSVG";
+//import Head from "./headSVG";
+//import Mouth from "./mouthSVG";
+//import Nose from "./noseSVG";
+
+const SvgContainerDiv = styled.svg`
   border: 0.0625rem solid black;
   border-radius: 0.25rem;
   width: 30rem;
@@ -24,26 +25,28 @@ const CanvasDiv = styled.svg`
 `;
 
 const SvgSketch = ({ seed }: { seed: number }): JSX.Element => {
-  const rnd = new RND(parseInt((9998 * seed).toString()));
+  //const rnd = new RND(parseInt((9998 * seed).toString()));
 
-  let roll = rnd.rb(0, 1);
-  const hasHat = roll < 0.8;
+  //let roll = rnd.rb(0, 1);
+  //const hasHat = roll < 0.8;
 
-  roll = rnd.rb(0, 1);
-  const hasArms = roll < 0.2;
+  //roll = rnd.rb(0, 1);
+  //const hasArms = roll < 0.2;
 
-  roll = rnd.rb(0, 1);
-  const hasGlasses = roll < 0.1;
+  //roll = rnd.rb(0, 1);
+  //const hasGlasses = roll < 0.1;
 
+  const inner = Background({seed});
+  console.log('inner', inner);
   return (
-    <CanvasDiv
+    <SvgContainerDiv
       key="0"
       xmlns="http://www.w3.org/2000/svg"
       xmlnsXlink="http://www.w3.org/1999/xlink"
       viewBox="0 0 800 800"
+      dangerouslySetInnerHTML={{ __html: inner, }}
     >
-      <Background seed={seed} />
-      <Body seed={seed} />
+      {/*<Body seed={seed} />
       <Head seed={seed} />
       {hasHat && <Hat seed={seed} />}
       <Eyes seed={seed} />
@@ -52,8 +55,8 @@ const SvgSketch = ({ seed }: { seed: number }): JSX.Element => {
       <Beard seed={seed} />
       <Mouth seed={seed} />
       {hasGlasses && <Glasses seed={seed} />}
-      <Nose seed={seed} />
-    </CanvasDiv>
+      <Nose seed={seed} /> */}
+    </SvgContainerDiv>
   );
 };
 
