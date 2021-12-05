552f5f51d72dfaef7d583f53c5a382e1b9c15959
r0mille <ke4mtg+r0mille@gmail.com>
2021-12-05 16:15:01 -0700

    script makes pngs now

--- scripts/generateCollection.ts
+++ scripts/generateCollection.ts
@@ -1,29 +1,49 @@
 import fs from "fs";
 import path from "path";
+import { Command } from "commander";
+import sharp from "sharp";
 
 import { getSVGMap } from "../components/image_generation/common";
-import { RagingSantaSVGString, RagingSantaTraits } from "../components/image_generation/svg-sketch";
+import {
+  RagingSantaSVGString,
+  RagingSantaTraits
+} from "../components/image_generation/svg-sketch";
 
-const MAX_SUPPLY = 100;
+const program = new Command();
+program.option("-q, --quantity <quantity>", "number to generate");
 
+program.parse(process.argv);
 
-var ds = new Date()
-var folderName = `${ds.getFullYear()}-${ds.getMonth()}-${ds.getDate()}-${ds.getHours()}-${ds.getMinutes()}-${ds.getSeconds()}`;
+const { quantity } = program.opts();
+function main(quantity: number): void {
+  var ds = new Date();
+  var folderName = `${ds.getFullYear()}-${ds.getMonth()}-${ds.getDate()}-${ds.getHours()}-${ds.getMinutes()}-${ds.getSeconds()}`;
 
-fs.mkdir(folderName, (err) => {
-  if (err) {
+  fs.mkdir(folderName, err => {
+    if (err) {
       throw err;
-  }
-  const svgMap = getSVGMap(fs, path);
-  for (let i = 0; i < MAX_SUPPLY; i++) {
-    let svgPath = path.join(folderName, `${i}.svg`)
-    fs.writeFileSync(svgPath, RagingSantaSVGString(i / 1000, svgMap));
+    }
+    const svgMap = getSVGMap(fs, path);
+    for (let i = 0; i < quantity; i++) {
+      let svgPath = path.join(folderName, `${i}.svg`);
+      const svgString = RagingSantaSVGString(i / 1000, svgMap);
+      fs.writeFileSync(svgPath, svgString);
 
-    let jsonPath = path.join(folderName, `${i}.json`)
-    fs.writeFileSync(jsonPath, JSON.stringify(RagingSantaTraits(i / 1000)));
-  }
+      let jsonPath = path.join(folderName, `${i}.json`);
+      fs.writeFileSync(jsonPath, JSON.stringify(RagingSantaTraits(i / 1000)));
+      const inputBuffer = Buffer.from(svgString);
+      let pngPath = path.join(folderName, `${i}.png`);
+      sharp(inputBuffer).resize(400, 400).toFile(pngPath);
+    }
 
-  console.log("Directory is created.");
-});
+    console.log("Directory is created.");
+  });
+}
 
-export {}
+if (quantity) {
+  main(quantity);
+} else {
+  console.warn("No Quantity Provided. Script Ending");
+}
+
+export {};
