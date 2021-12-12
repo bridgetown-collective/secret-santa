0cda53f1e8bef63e38c36341eafa8b56f7765a4e
r0mille <ke4mtg+r0mille@gmail.com>
2021-12-12 10:00:25 -0700

    adds whitepaper page

---
+++ pages/whitepaper.tsx
@@ -0,0 +1,13 @@
+export default function Whitepaper() {
+  return (
+    <div className="flex-col justify-center flex-wrap mt-32 mb-24 w-full">
+      <span
+        className="text-5xl xl:text-7xl tracking-tight text-shadow-green"
+        style={{ fontFamily: "LogoFont" }}
+      >
+        Whitepaper
+      </span>
+      <p>Watch this Space!</p>
+    </div>
+  );
+}
