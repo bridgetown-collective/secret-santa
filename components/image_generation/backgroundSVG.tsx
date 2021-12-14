8d951a9c022d34c5ce62b6cac4934c1944d1a75b
r0mille <ke4mtg+r0mille@gmail.com>
2021-12-14 13:23:06 -0700

    rebalance backgrounds

--- components/image_generation/backgroundSVG.tsx
+++ components/image_generation/backgroundSVG.tsx
@@ -11,34 +11,34 @@ export default function Background(
 
   let variant = "forest_8_bit";
   switch (true) {
-    case roll < 0.125:
+    case roll < 0.11:
       variant = "green_textured";
       break;
-    case roll < 0.25:
+    case roll < 0.11 * 2:
       variant = "light_yellow";
       break;
-    case roll < 0.375:
-      variant = "orange_pink_no_texture";
-      break;
-    case roll < 0.5:
+    case roll < 0.11 * 3:
       variant = "yellow_no_texture";
       break;
-    case roll < 0.625:
+    case roll < 0.11 * 4:
       variant = "pink_textured";
       break;
-    case roll < 0.75:
+    case roll < 0.11 * 5:
       variant = "blue_textured";
       break;
-    case roll < 0.80:
+    case roll < 0.65:
+      variant = "orange_pink_no_texture";
+      break;
+    case roll < 0.65 + 0.07 * 1:
       variant = "rainbow";
       break;
-    case roll < 0.85:
+    case roll < 0.65 + 0.07 * 2:
       variant = "snowflakes";
       break;
-    case roll < 0.90:
+    case roll < 0.80 + 0.08 * 1:
       variant = "forest";
       break;
-    case roll < 0.95:
+    case roll < 0.80 + 0.08 * 2:
       variant = "mountains";
       break;
     case roll < 0.966:
