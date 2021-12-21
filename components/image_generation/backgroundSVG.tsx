628dd86d9419d9832dd826cb4e75f7f6a0016227
r0mille <ke4mtg+r0mille@gmail.com>
2021-12-21 11:50:55 -0700

    updates backgrounds

--- components/image_generation/backgroundSVG.tsx
+++ components/image_generation/backgroundSVG.tsx
@@ -17,35 +17,41 @@ export default function Background(
     case roll < 0.11 * 2:
       variant = "light_yellow";
       break;
-    case roll < 0.11 * 3:
+    case roll < 0.22 + 0.10 * 1:
       variant = "yellow_no_texture";
       break;
-    case roll < 0.11 * 4:
+    case roll < 0.22 + 0.10 * 2:
       variant = "pink_textured";
       break;
-    case roll < 0.11 * 5:
+    case roll < 0.22 + 0.10 * 3:
       variant = "blue_textured";
       break;
-    case roll < 0.65:
+    case roll < 0.22 + 0.10 * 4:
       variant = "orange_pink_no_texture";
       break;
-    case roll < 0.65 + 0.07 * 1:
-      variant = "rainbow";
-      break;
-    case roll < 0.65 + 0.07 * 2:
+    case roll < 0.62 + 0.08 * 1:
       variant = "snowflakes";
       break;
-    case roll < 0.80 + 0.08 * 1:
+    case roll < 0.62 + 0.08 * 2:
       variant = "forest";
       break;
-    case roll < 0.80 + 0.08 * 2:
+    case roll < 0.78 + 0.07 * 1:
+      variant = "rainbow";
+      break;
+    case roll < 0.78 + 0.07 * 2:
       variant = "mountains";
       break;
-    case roll < 0.966:
+    case roll < 0.92 + 0.0166:
+      variant = "hell";
+      break;
+    case roll < 0.92 + 0.0166 * 2:
+      variant = "stars_space";
+      break;
+    case roll < 0.92 + 0.0166 * 3:
       variant = "camo_white_fill";
       break;
-    case roll < 0.983:
-      variant = "not_space_invaders";
+    case roll < 0.92 + 0.0166 * 4:
+      variant = "alien_8_bit";
       break;
     default:
   }
